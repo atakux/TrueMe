ae8b8c4c003e0ba56ba88d7fdd7da35f4de19a4e
Angela DeLeo <rockyangela5@gmail.com>
2024-03-02 18:57:41 -0800

    added comments. ready for View Routine

--- src/components/RoutinesScreens/AddRoutine.js
+++ src/components/RoutinesScreens/AddRoutine.js
@@ -4,7 +4,6 @@ import { useNavigation } from '@react-navigation/native';
 import { collection, addDoc } from 'firebase/firestore';
 import { FIRESTORE_DB } from '../../../firebase'; 
 import { useAuth } from '../../utils/AuthContext';
-import { useRoutineContext } from '../../utils/RoutineContext';
 import { loadFonts } from '../../utils/FontLoader';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import DraggableFlatList from 'react-native-draggable-flatlist';
@@ -13,37 +12,51 @@ const screenWidth = Dimensions.get('window').width;
 
 const AddRoutine = ({ route }) => {
     const navigation = useNavigation();
-    const [routineName, setRoutineName] = useState('');
-    const [selectedDays, setSelectedDays] = useState([]);
-    const [steps, setSteps] = useState([]);
-    const [stepName, setStepName] = useState('');
-    const [selectedStep, setSelectedStep] = useState(null); // State to keep track of selected step
     const user = useAuth(); 
+    const [fontLoaded, setFontLoaded] = useState(false);
     
+    // Routines related consts
+    const [routineName, setRoutineName] = useState('');
     const { updateDailyRoutines } = route.params;
     const [errors, setErrors] = useState([]);
-    const [stepErrors, setStepErrors] = useState([]);
-    const [selectedDaysCount, setSelectedDaysCount] = useState(0); // State to keep track of selected days count
+    
+    // Days consts
+    const [selectedDays, setSelectedDays] = useState([]);
+    const [selectedDaysCount, setSelectedDaysCount] = useState(0); 
+
+    // Steps consts
+    const [steps, setSteps] = useState([]);
+    const [stepName, setStepName] = useState('');
+    const [selectedStep, setSelectedStep] = useState(null);
+    const [stepErrors, setStepErrors] = useState([]); // Errors for modulars
+
     const [modalVisible, setModalVisible] = useState(false);
 
     // Load fonts
     useEffect(() => {
       const loadAsyncData = async () => {
         await loadFonts();
+        setFontLoaded(true);
       };
       loadAsyncData();
-    }, []);
+    }, [user]);
 
+    // Update selected days count whenever selectedDays changes
     useEffect(() => {
-      // Update selected days count whenever selectedDays changes
       setSelectedDaysCount(selectedDays.length);
     }, [selectedDays]);
+
+    if (!fontLoaded || user === null) {
+      // Font is still loading or user is not logged in
+      return null;
+    }
 
     // Add routine 
     const handleAddRoutine = async () => {
       try {
         setErrors([]);
 
+        // Validate inputs
         if (routineName === '') {
           console.log('DEBUG: Routine title cannot be empty');
           setErrors((prevErrors) => [...prevErrors, 'Please enter a routine name']);
@@ -56,7 +69,7 @@ const AddRoutine = ({ route }) => {
           console.log('DEBUG: Steps cannot be empty');
           setErrors((prevErrors) => [...prevErrors, 'Please add at least one step']);
           return;
-        }
+        } 
 
         // Add routine to user's routines collection in Firestore
         const routinesDocRef = collection(FIRESTORE_DB, 'users', user.uid, 'routines');
@@ -65,18 +78,21 @@ const AddRoutine = ({ route }) => {
           days: selectedDays,
           steps: steps,
         });
+
         // Call the update function passed from HomeScreen to update dynamically
         updateDailyRoutines({ id: docRef.id, title: routineName, days: selectedDays });
 
-        console.log('DEBUG: Routine', routineName, 'added successfully');
+        console.log('DEBUG: ', routineName, 'added successfully');
 
         // Navigate back to HomeScreen
         navigation.goBack();
+
       } catch (error) {
         console.error('DEBUG: Error adding routine: ', error);
       }
     }; // End handleAddRoutine
 
+    // This function generates the header text for the steps.
     const generateStepsHeaderText = () => {
       const stepCount = steps.length;
       if (stepCount === 0) {
@@ -86,10 +102,12 @@ const AddRoutine = ({ route }) => {
       }
     };
 
+    // Click handler for each step
     const handleClickStep = (step) => {
       setSelectedStep(step);
     };
 
+    // Add step 
     const handleAddStep = (stepName) => {
       if (!stepName.trim()) {
         console.log('DEBUG: Step name cannot be empty');
@@ -104,6 +122,7 @@ const AddRoutine = ({ route }) => {
       }
     };
     
+    // Edit step
     const handleEditStep = () => {
       if (!stepName.trim()) {
         // If step name is empty, set it to the previous value
@@ -122,6 +141,7 @@ const AddRoutine = ({ route }) => {
       setStepName(''); // Reset stepName after editing
     };
     
+    // Delete step 
     const handleDeleteStep = () => {
       const updatedSteps = steps.filter(step => step !== selectedStep);
       setSteps(updatedSteps);
@@ -129,6 +149,7 @@ const AddRoutine = ({ route }) => {
       setStepName(''); // Reset stepName after deleting step
     };
 
+    // Toggle modal visibility
     const toggleModal = () => {
       setModalVisible(!modalVisible);
     };
@@ -150,7 +171,9 @@ const AddRoutine = ({ route }) => {
                     </Text>
             ))}
             
+            {/* Main content container for Add Routine screen */}
             <View style={styles.contentContainer}>
+
                 {/* Routine name user must enter */}
                 <Text style={styles.inputLabel}>Name</Text>
 
@@ -191,8 +214,10 @@ const AddRoutine = ({ route }) => {
                     <Image source={require('../../../assets/icons/plus.png')}/>
                 </TouchableOpacity>
 
+                {/* Directions for reordering steps */}
                 <Text style={styles.tinyText}>Press and hold to reorder</Text>
 
+                {/* Modal for adding steps */}
                 <Modal
                   animationType="slide"
                   transparent={true}
@@ -245,8 +270,6 @@ const AddRoutine = ({ route }) => {
                   onDragEnd={({ data }) => setSteps(data)} // Update steps state after reordering
                 />
 
-
-                {/* Modal for editing/deleting selected step */}
                 {/* Modal for editing/deleting selected step */}
                 <Modal
                   animationType="slide"
@@ -278,9 +301,9 @@ const AddRoutine = ({ route }) => {
                   </TouchableOpacity>
                 </Modal>
 
-            </View>
+            </View> 
+            {/* End of contentContainer */}
             
-
             {/* Add Routine Button */}
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.addRoutineButton} onPress={handleAddRoutine}>
@@ -295,17 +318,17 @@ const AddRoutine = ({ route }) => {
   
   const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#FAFAFA",
-        width: "100%",
+      flex: 1,
+      backgroundColor: "#FAFAFA",
+      width: "100%",
     }, // End of container
 
     title: {
-        fontSize: 32,
-        fontFamily: "Sofia-Sans",
-        textAlign: "left",
-        marginTop: 20,
-        marginLeft: 20,
+      fontSize: 32,
+      fontFamily: "Sofia-Sans",
+      textAlign: "left",
+      marginTop: 20,
+      marginLeft: 20,
     }, // End of title
 
     closeButton: {
@@ -314,85 +337,79 @@ const AddRoutine = ({ route }) => {
       right: 20,
       marginRight: 10,
       zIndex: 999,
-    },
+    }, // End of closeButton
 
     contentContainer: {
-        flex: 1,
-        alignSelf: "flex-start",
-        margin: 20,
-      }, // End of contentContainer
+      flex: 1,
+      alignSelf: "flex-start",
+      margin: 20,
+    }, // End of contentContainer
 
     buttonContainer: {
-        width: "110%",
-        backgroundColor: "#FFFFFF",
-        
-        // Border
-        borderRadius: 75,
-        borderWidth: 2,
-        borderBottomColor: "rgba(0, 0, 0, 0)",
-        borderLeftColor: "rgba(0, 0, 0, 0)",
-        borderRightColor: "rgba(0, 0, 0, 0)",
-        borderColor: "rgba(0, 0, 0, 0.1)",
-        
-        // Alignment
-        alignSelf: "center",
-        alignItems: "center",
-        justifyContent: "center",
-        paddingTop: 20,
+      width: "110%",
+      backgroundColor: "#FFFFFF",
+      
+      // Border
+      borderRadius: 75,
+      borderWidth: 2,
+      borderBottomColor: "rgba(0, 0, 0, 0)",
+      borderLeftColor: "rgba(0, 0, 0, 0)",
+      borderRightColor: "rgba(0, 0, 0, 0)",
+      borderColor: "rgba(0, 0, 0, 0.1)",
+      
+      // Alignment
+      alignSelf: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 20,
     }, // End of buttonContainer    
 
     inputName: {
-        width: screenWidth - 40,
-        height: 65,
-        backgroundColor: "#FFFFFF",
-        borderRadius: 20,
-        alignSelf: "center",
-        marginBottom: 40,
-        fontFamily: "Sofia-Sans",
-        fontSize: 23,
-        padding: 20,
-        // Style depending on iOS vs Android
-        ...Platform.select({
-          ios: {
-            shadowColor: "rgba(0, 0, 0, 0.25)",
-            shadowOffset: {
-              width: 2,
-              height: 4,    
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
+      width: screenWidth - 40,
+      height: 65,
+      backgroundColor: "#FFFFFF",
+      borderRadius: 20,
+      alignSelf: "center",
+      marginBottom: 40,
+      fontFamily: "Sofia-Sans",
+      fontSize: 23,
+      padding: 20,
+      // Style depending on iOS vs Android
+      ...Platform.select({
+        ios: {
+          shadowColor: "rgba(0, 0, 0, 0.25)",
+          shadowOffset: {
+            width: 2,
+            height: 4,    
           },
-          android: {
-            elevation: 5,
-          },
-        }),
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+        },
+        android: {
+          elevation: 5,
+        },
+      }),
     }, // End of input
 
     inputLabel: {
-        fontSize: 24,
-        fontFamily: "Sofia-Sans",
-        color: "#535353",
-        marginBottom: 15,
-        marginTop: 5,
-        textAlign: "left",
+      fontSize: 24,
+      fontFamily: "Sofia-Sans",
+      color: "#535353",
+      marginBottom: 15,
+      marginTop: 5,
+      textAlign: "left",
     }, // End of inputLabel
 
     errorMessage: {
-        fontSize: 14,
-        fontFamily: "Sofia-Sans",
-        color: "red",
-        textAlign: "center",
-        alignSelf: "flex-start",
-        marginTop: 5,
-        marginBottom: 5,
-        marginLeft: 15,
+      fontSize: 14,
+      fontFamily: "Sofia-Sans",
+      color: "red",
+      textAlign: "center",
+      alignSelf: "flex-start",
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 15,
     }, // End of errorMessage
-
-    dayText: {
-        fontSize: 16,
-        fontFamily: "Sofia-Sans",
-        color: "#356553",
-    },
 
     tinyText: {
       fontSize: 16,
@@ -401,33 +418,33 @@ const AddRoutine = ({ route }) => {
       textAlign: "right",
       marginBottom: 13,
       marginRight: 2,
-    },
+    }, // End of tinyText
 
     // Buttons
     buttons: {
-        width: screenWidth - 40,
-        height: 45,
-        backgroundColor: "#D0F2DA",
-        borderRadius: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        alignSelf: "center",
-        marginTop: 2,
-        marginBottom: 10,
-        opacity: 0.85,
+      width: screenWidth - 40,
+      height: 45,
+      backgroundColor: "#D0F2DA",
+      borderRadius: 100,
+      justifyContent: "center",
+      alignItems: "center",
+      alignSelf: "center",
+      marginTop: 2,
+      marginBottom: 10,
+      opacity: 0.85,
     }, // End of buttons
 
     addRoutineButton: {
-        width: "85%",
-        height: 60,
-        backgroundColor: "#D0F2DA",
-        borderRadius: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        alignSelf: "center",
-        marginTop: 2,
-        marginBottom: 5,
-      }, // End of addRoutinebutton
+      width: "85%",
+      height: 60,
+      backgroundColor: "#D0F2DA",
+      borderRadius: 100,
+      justifyContent: "center",
+      alignItems: "center",
+      alignSelf: "center",
+      marginTop: 2,
+      marginBottom: 5,
+    }, // End of addRoutinebutton
     
     buttonText: {
       fontSize: 28,
@@ -436,32 +453,29 @@ const AddRoutine = ({ route }) => {
       textAlign: "center",
     }, // End of buttonText
 
-    addStepButton: {
-      fontSize: 22,
-      fontFamily: "Sofia-Sans",
-      color: "#64BBA1",
-      textAlign: "center",
-      marginTop: 5,
-      
-    },
-
     // Days
+    dayText: {
+      fontSize: 16,
+      fontFamily: "Sofia-Sans",
+      color: "#356553",
+    }, // End of dayText
+
     daysContainer: {
       flexDirection: 'row',
       justifyContent: 'space-between',
       marginBottom: 10,
-    },
+    }, // End of daysContainer
 
     dayButton: {
       borderWidth: 1,
       borderColor: '#ccc',
       padding: 10,
       borderRadius: 5,
-    },
+    }, // End of dayButton
 
     selectedDayButton: {
       backgroundColor: '#64BBA1',
-    },
+    }, // End of selectedDayButton
 
     // Modal
     modalContainer: {
@@ -469,7 +483,7 @@ const AddRoutine = ({ route }) => {
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
+    }, // End of modalContainer
 
     modalContent: {
       backgroundColor: '#fff',
@@ -477,7 +491,7 @@ const AddRoutine = ({ route }) => {
       borderRadius: 10,
       alignItems: 'center',
       justifyContent: 'center',
-    },
+    }, // End of modalContent
 
     modalInput: {
       borderColor: 'gray',
@@ -503,7 +517,7 @@ const AddRoutine = ({ route }) => {
           fontSize: 18,
         },
       }),
-    },
+    }, // End of modalInput
     
     // Steps
     stepContainer: {
@@ -522,14 +536,14 @@ const AddRoutine = ({ route }) => {
       
       marginTop: 5,
       marginHorizontal: 10,
-    },
+    }, // End of stepContainer
 
     stepText: {
-        fontSize: 18,
-        fontFamily: "Sofia-Sans",
-        color: "#356553",
-        marginLeft: 15,
-    },
+      fontSize: 18,
+      fontFamily: "Sofia-Sans",
+      color: "#356553",
+      marginLeft: 15,
+    }, // End of stepText
 
     editStepButton: {
       fontSize: 18,
@@ -538,7 +552,7 @@ const AddRoutine = ({ route }) => {
       textAlign: "center",
       marginTop: 10,
       marginBottom: 5,
-    },
+    }, // End of editStepButton
     
     deleteStepButton: {
       fontSize: 18,
@@ -547,7 +561,15 @@ const AddRoutine = ({ route }) => {
       textAlign: "center",
       marginTop: 5,
       marginBottom: 5,
-    },
+    }, // End of deleteStepButton
+
+    addStepButton: {
+      fontSize: 22,
+      fontFamily: "Sofia-Sans",
+      color: "#64BBA1",
+      textAlign: "center",
+      marginTop: 5,
+    }, // End of addStepButton
     
   });
 
