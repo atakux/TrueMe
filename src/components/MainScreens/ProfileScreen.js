6652535cdac7829fde018b3c62db93bf80145245
Angela DeLeo <rockyangela5@gmail.com>
2024-03-06 20:31:29 -0800

    fixed profile screen bugs

--- src/components/MainScreens/ProfileScreen.js
+++ src/components/MainScreens/ProfileScreen.js
@@ -9,7 +9,9 @@ import { FIREBASE_AUTH } from "../../../firebase";
 
 import { loadFonts } from '../../utils/FontLoader'; 
 import { useAuth } from '../../utils/AuthContext';
-import { fetchBannerImage, fetchProfileImage } from '../../utils/FirestoreDataService';
+
+import * as ImagePicker from 'expo-image-picker';
+import { fetchBannerImage, fetchProfileImage, uploadProfileImage, uploadBannerImage } from '../../utils/FirestoreDataService';
 
 const ProfileScreen = () => {
   const auth = FIREBASE_AUTH;
@@ -79,39 +81,105 @@ const ProfileScreen = () => {
     }
   };
 
+  const pickBannerImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [10, 10],
+      quality: 1,
+      canceled: false
+    });
+  
+    console.log(result);
+  
+    if (!result.canceled) {
+      setLoading(true); // Set loading to true while uploading image
+      setBannerImage(result.assets[0].uri);
+      // Call uploadBannerImage function to save the image to Firestore
+      try {
+        await uploadBannerImage(user.uid, result.assets[0].uri);
+      } catch (error) {
+        console.error('Error uploading image: ', error);
+      } finally {
+        setLoading(false); // Set loading back to false after upload completes
+      }
+    }
+  };
+
+  const pickProfileImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [10, 10],
+      quality: 1,
+      canceled: false
+    });
+  
+    console.log(result);
+  
+    if (!result.canceled) {
+      setLoading(true); // Set loading to true while uploading image
+      setProfileImage(result.assets[0].uri);
+      // Call uploadBannerImage function to save the image to Firestore
+      try {
+        await uploadProfileImage(user.uid, result.assets[0].uri);
+      } catch (error) {
+        console.error('Error uploading image: ', error);
+      } finally {
+        setLoading(false); // Set loading back to false after upload completes
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.imagesContainer}>
-        {loading ? ( // Conditionally rendering loading indicator
+  {loading ? ( // Conditionally rendering loading indicator
+    <ActivityIndicator size="large" color="#64BBA1" style={styles.loadingIndicator} />
+  ) : bannerImage ? (
+    <View style={styles.imageContainerBanner}>
+      <Image source={{ uri: bannerImage }} style={styles.imageBanner} />
+      <View>
+        {loading ? (
           <ActivityIndicator size="large" color="#64BBA1" style={styles.loadingIndicator} />
-        ) : bannerImage ? (
-          <View style={styles.imageContainerBanner}>
-            <Image source={{ uri: bannerImage }} style={styles.imageBanner} />
-            <View>
-               {loading ? (
-                 <ActivityIndicator size="large" color="#64BBA1" style={styles.loadingIndicator} />
-               ) : profileImage ? (
-                 <View style={styles.profileImageContainer}>
-                   <Image source={{ uri: profileImage }} style={styles.profileImage} />
-                 </View>
-               ) : (
-                <TouchableOpacity onPress={pickProfileImage}>
-                 <View style={styles.profileImageContainer}>
-                   <Image source={require('../../../assets/icons/add-photo.png')} style={styles.selectProfileIcon}/>
-                 </View>
-                </TouchableOpacity>
-               )}
-            </View>
+        ) : profileImage ? (
+          <View style={styles.profileImageContainer}>
+            <Image source={{ uri: profileImage }} style={styles.profileImage} />
           </View>
         ) : (
-          <View style={styles.selectBannerImageContainer}>
-            <TouchableOpacity onPress={pickBannerImage}>
-              <Image source={require('../../../assets/icons/select.png')} style={styles.selectBannerIcon} />
-              <Text style={styles.imageButtonText}>Select Banner Image</Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity onPress={pickProfileImage}>
+            <View style={styles.profileImageContainer}>
+              <Image source={require('../../../assets/icons/add-photo.png')} style={styles.selectProfileIcon}/>
+            </View>
+          </TouchableOpacity>
         )}
       </View>
+    </View>
+  ) : (
+    <View style={styles.selectBannerImageContainer}>
+      <TouchableOpacity onPress={pickBannerImage}>
+        <Image source={require('../../../assets/icons/select.png')} style={styles.selectBannerIcon} />
+        <Text style={styles.imageButtonText}>Select Banner Image</Text>
+      </TouchableOpacity>
+      {loading ? (
+        <ActivityIndicator size="large" color="#64BBA1" style={styles.loadingIndicator} />
+      ) : profileImage ? (
+        <TouchableOpacity onPress={pickProfileImage}>
+          <View style={styles.profileImageContainer}>
+            <Image source={{ uri: profileImage }} style={styles.profileImage} />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={pickProfileImage}>
+          <View style={styles.profileImageContainer}>
+            <Image source={require('../../../assets/icons/add-photo.png')} style={styles.selectProfileIcon}/>
+          </View>
+        </TouchableOpacity>
+      )}
+    </View>
+  )}
+</View>
+
       
       <View>
         <Text style={styles.mainText}>{user.displayName}</Text>
@@ -180,8 +248,9 @@ const styles = StyleSheet.create({
 
   buttonsContainer: {
     flex: 1,
+    flexGrow: 2,
     width: "100%",
-    marginBottom: 500,
+    marginBottom: 600,
   }, // End of buttonsContainer
 
   imagesContainer: {
@@ -204,6 +273,18 @@ const styles = StyleSheet.create({
     textAlign: "center",
     letterSpacing: 3.5,
     marginBottom: 5,
+
+    ...Platform.select({
+      ios: {
+        marginTop: 10,
+      },
+
+      android: {
+        marginTop: 10,
+      }
+
+    }),
+    
   }, // End of mainText
 
   loadingIndicator: {
@@ -217,16 +298,19 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     borderRadius: 100,
+    borderColor: '#7FB876',
+    borderWidth: 1,
+    backgroundColor: '#EBF5F5',
     alignContent: 'center',
     justifyContent: 'center',
     alignSelf: "center",
 
     ...Platform.select({
       ios: {
-        top: -80,
+        top: -5,
       },
       android: {
-        top: -80,
+        top: -5,
       },
     }),
   }, // End of profileImageContainer
@@ -235,35 +319,6 @@ const styles = StyleSheet.create({
     width: 64,
     height: 64,
     alignSelf: "center",
-  }, // End of selectProfileIcon
-
-  selectProfileIconTiny: {
-    width: 24,
-    height: 24,
-    alignSelf: "center",
-    alignContent: "center",
-    justifyContent: "center",
-  }, // End of selectProfileIcon
-
-  selectProfileImageTinyIconButton : {
-    position: 'absolute',
-    borderRadius: 50,
-    backgroundColor: '#EBF5F5',
-    borderWidth: 1,
-    borderColor: '#7FB876',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignContent: 'center',
-    padding: 10,
-
-    ...Platform.select({
-      ios: {
-        bottom: 2,
-      },
-      android: {
-        bottom: 2,  
-      },
-    }),
   }, // End of selectProfileIcon
 
   profileImage: {
@@ -275,53 +330,28 @@ const styles = StyleSheet.create({
   // Banner
   selectBannerImageContainer: {
     width: "100%",
-    height: 200,
     backgroundColor: '#EBF5F5',
     borderWidth: 1,
     borderColor: '#7FB876',
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: "center",
+    ...Platform.select({
+      ios: {
+        height: 200,
+      },
+      android: {
+        marginTop: 40,
+        height: 200,
+      },
+    }),
   }, // End of selectBannerImageContainer
-
-  selectBannerIconTiny: {
-    width: 32,
-    height: 32,
-    alignSelf: "center",
-  }, // End of selectBannerIcon
 
   selectBannerIcon: {
     width: 64,
     height: 64,
     alignSelf: "center",
   }, // End of selectBannerIcon
-
-  selectBannerImageTinyIconButton: {
-    position: 'absolute',
-    borderRadius: 50,
-    backgroundColor: '#EBF5F5',
-    borderWidth: 1,
-    borderColor: '#7FB876',
-    paddingLeft: 12,
-    paddingTop: 10,
-    paddingBottom: 9,
-    paddingRight: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignContent: 'center',
-
-    ...Platform.select({
-      ios: {
-        bottom: -20,
-        right: 5,
-      },
-
-      android: {
-        bottom: -65,
-        right: 5,
-      },
-    }),
-  }, // End of selectBannerImageTinyIconButton
 
   imageContainerBanner: {
     position: 'relative',
@@ -403,6 +433,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Sofia-Sans',
     color: '#64BBA1',
     textAlign: "center",
+    marginBottom: 15,
   }, // End of buttonText
 });
 
