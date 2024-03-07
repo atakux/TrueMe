4418e3d74f5d680aa5263991fb4e273c7fdd9fbc
Angela DeLeo <rockyangela5@gmail.com>
2024-03-07 12:10:24 -0800

    firebase should reset routines step completion status daily now

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,30 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.dailyReset = functions.pubsub.schedule('every 24 hours').onRun(async (context) => {
-    const firestore = admin.firestore();
-    const usersCollectionRef = firestore.collection('users');
+exports.midnightReset = functions.pubsub
+    .schedule("0 0 * * *") // Run at midnight every day
+    .onRun(async () => {
+      const firestore = admin.firestore();
+      const usersCollectionRef = firestore.collection("users");
 
-    try {
+      try {
         // Fetch all users
         const usersSnapshot = await usersCollectionRef.get();
         usersSnapshot.forEach(async (userDoc) => {
-            const routinesCollectionRef = userDoc.ref.collection('routines');
-            // Fetch all routines for the current user
-            const routinesSnapshot = await routinesCollectionRef.get();
-            routinesSnapshot.forEach(async (routineDoc) => {
-                // Reset the stepCompletionStatus array to all false
-                await routineDoc.ref.update({
-                    stepCompletionStatus: Array(routineDoc.data().steps.length).fill(false)
-                });
+          const routinesCollectionRef = userDoc.ref.collection("routines");
+          // Fetch all routines for the current user
+          const routinesSnapshot = await routinesCollectionRef.get();
+          routinesSnapshot.forEach(async (routineDoc) => {
+            // Reset the stepCompletionStatus array to all false
+            await routineDoc.ref.update({
+              // eslint-disable-next-line max-len
+              stepCompletionStatus: Array(routineDoc.data().steps.length).fill(false),
             });
+          });
         });
-        console.log('Step completion status reset successfully.');
-    } catch (error) {
-        console.error('Error resetting step completion status:', error);
-    }
-});
+        console.log("Step completion status reset successfully.");
+      } catch (error) {
+        console.error("Error resetting step completion status:", error);
+      }
+    });
