f1dfe66f38de184df114355adf918e065204abb7
RomanSaddi <Romansaddi@gmail.com>
2024-04-07 13:12:59 -0700

    added daily affirmations to launch screen

--- src/authentication/LaunchScreen.js
+++ src/authentication/LaunchScreen.js
@@ -1,23 +1,24 @@
-// Launch Screen
-
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, SafeAreaView, View, Text, Button, TouchableOpacity, Image } from 'react-native';
-
-
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
 import * as Font from 'expo-font';
+import { loadFonts } from '../utils/FontLoader'; // If needed
 
-import { loadFonts } from '../utils/FontLoader'; 
+// Importing JSON data
+import affirmations from './affirmations.json'; // Adjust the file path as needed
 
 const LaunchScreen = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
+  const [affirmation, setAffirmation] = useState('');
   const navigation = useNavigation();
 
   useEffect(() => {
     const loadAsyncData = async () => {
-      await loadFonts();
+      await loadFonts(); // If needed
       setFontLoaded(true);
+      // Select a random affirmation
+      const randomIndex = Math.floor(Math.random() * affirmations.length);
+      setAffirmation(affirmations[randomIndex]);
     };
 
     loadAsyncData();
@@ -29,94 +30,57 @@ const LaunchScreen = () => {
   }
 
   return (
-    // Actual Launch Screen
     <SafeAreaView style={styles.container}>
-
-        {/* Top flowers */}
-        <Image source={require('../../assets/images/top_flowers.png')} style={styles.topFlowers} />
-
-        {/* Title and Affirmation Text */}
-        <View style={{alignItems: 'center', marginTop: 50 }}>
-            {/* Title */}
-            <Text style={styles.trueMeTitle}>
-                {"TrueMe"}
-            </Text> 
-
-            {/* Daily Affirmation Text 
-            TODO: Get Daily Affirmation from API */}
-            <Text style={styles.mainText}>
-                {"Insert Daily Affirmation Here!"}
-            </Text>
-
-        </View>
-
-        {/* Login and Sign up buttons */}
-        <View style={{alignItems: 'center', marginTop: 50}}>
-            
-            <TouchableOpacity
-                style={styles.buttons}
-                onPress={() => {
-                    // Navigate to LoginScreen
-                    navigation.navigate('LoginScreen');
-                }}
-                >
-                <Text style={styles.buttonText}>
-                    {"Login"}
-                </Text>
-            </TouchableOpacity>
-
-            {/* Spacer */}
-            <View style={{ height: 50 }} /> 
-
-
-            {/* Sign Up button will take you to sign up screen. */}
-            <TouchableOpacity
-                style={styles.buttons}
-                onPress={() => {
-                    // Navigate to SignupScreen
-                    navigation.navigate('SignupScreen');
-                }}
-                >
-                <Text style={styles.buttonText}>
-                    {"Sign Up"}
-                </Text>
-            </TouchableOpacity>
-
-        </View>
-
-        {/* Bottom flowers */}
-        <Image source={require('../../assets/images/bottom_flowers.png')} style={{marginTop: 60, marginBottom: 50, width: 285, height: 178}} />
-      
+      <Image source={require('../../assets/images/top_flowers.png')} style={styles.topFlowers} />
+      <View style={{ alignItems: 'center', marginTop: 50 }}>
+        <Text style={styles.trueMeTitle}>{"TrueMe"}</Text>
+        <Text style={styles.mainText}>{affirmation}</Text>
+      </View>
+      <View style={{ alignItems: 'center', marginTop: 50 }}>
+        <TouchableOpacity
+          style={styles.buttons}
+          onPress={() => {
+            // Navigate to LoginScreen
+            navigation.navigate('LoginScreen');
+          }}>
+          <Text style={styles.buttonText}>{"Login"}</Text>
+        </TouchableOpacity>
+        <View style={{ height: 50 }} />
+        <TouchableOpacity
+          style={styles.buttons}
+          onPress={() => {
+            // Navigate to SignupScreen
+            navigation.navigate('SignupScreen');
+          }}>
+          <Text style={styles.buttonText}>{"Sign Up"}</Text>
+        </TouchableOpacity>
+      </View>
+      <Image source={require('../../assets/images/bottom_flowers.png')} style={{ marginTop: 60, marginBottom: 50, width: 285, height: 178 }} />
     </SafeAreaView>
   );
 };
 
-// Styles
-
 const styles = StyleSheet.create({
   topFlowers: {
     ...Platform.select({
-        ios: {
-            marginLeft: 150, 
-            width: 245, 
-            height: 138,
-            marginTop: -52,
-        },
-
-        android: {
-            marginLeft: 120, 
-            width: 245, 
-            height: 138,
-        }
-    })    
+      ios: {
+        marginLeft: 150,
+        width: 245,
+        height: 138,
+        marginTop: -52,
+      },
+      android: {
+        marginLeft: 120,
+        width: 245,
+        height: 138,
+      },
+    }),
   },
-
   container: {
-    flex: 5,
+    flex: 1,
     backgroundColor: "#D4CCDD",
     width: "100%",
-  }, // End of container
-
+  },
   trueMeTitle: {
     fontSize: 64,
     fontFamily: 'Spirax-Regular',
@@ -124,15 +88,14 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 30,
     marginTop: 15,
-  }, // End of trueMeTitle
-
+  },
   mainText: {
     fontSize: 24,
     fontFamily: 'Sofia-Sans',
     color: '#000000',
     textAlign: "center",
-  }, // End of mainText
-
+    width: '90%',
+  },
   buttons: {
     width: 159,
     height: 82,
@@ -142,15 +105,13 @@ const styles = StyleSheet.create({
     borderColor: '#7FB876',
     justifyContent: 'center',
     alignItems: 'center',
-  }, // End of buttons
-
+  },
   buttonText: {
     fontSize: 32,
     fontFamily: 'Sofia-Sans',
     color: '#64BBA1',
     textAlign: "center",
-  }, // End of buttonText
-
+  },
 });
 
 export default LaunchScreen;
