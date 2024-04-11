b9eb0206616a0a990de6b78e57ce4f09bbcd5bc0
Angela DeLeo <rockyangela5@gmail.com>
2024-04-11 15:56:49 -0700

    fixed amount of steps in generated routines

--- src/utils/GenerateRoutine.js
+++ src/utils/GenerateRoutine.js
@@ -5,25 +5,25 @@ const skincareRoutines = {
       title: "Hydrating Skincare",
       steps: ["Cleanse with a gentle hydrating cleanser", "Apply a hydrating toner", "Use a rich moisturizer", "Apply a facial oil for extra hydration", "Finish with a sunscreen in the morning"],
       days: [0, 1, 2, 3, 4, 5, 6],
-      stepCompletionStatus: [false, false, false, false, false, false, false],
+      stepCompletionStatus: [false, false, false, false, false],
     },
     oily: {
       title: "Balancing Skincare",
       steps: ["Cleanse with a foaming cleanser to remove excess oil", "Apply a gentle exfoliating toner to unclog pores", "Use an oil-free moisturizer", "Apply a mattifying sunscreen in the morning"],
       days: [0, 1, 2, 3, 4, 5, 6],
-      stepCompletionStatus: [false, false, false, false, false, false, false],
+      stepCompletionStatus: [false, false, false, false],
     },
     normal: {
       title: "Basic Skincare",
       steps: ["Cleanse with a gentle cleanser", "Apply a gentle exfoliating toner", "Use a rich moisturizer", "Apply a facial oil for extra hydration", "Finish with a sunscreen in the morning"],
       days: [0, 1, 2, 3, 4, 5, 6],
-      stepCompletionStatus: [false, false, false, false, false, false, false],
+      stepCompletionStatus: [false, false, false, false, false],
     },
     combination: {
       title: "Combination Skincare",
       steps: ["Cleanse with a gentle cleanser", "Apply a gentle exfoliating toner", "Use a rich moisturizer", "Apply a facial oil for extra hydration", "Finish with a sunscreen in the morning"],
       days: [0, 1, 2, 3, 4, 5, 6],
-      stepCompletionStatus: [false, false, false, false, false, false, false],
+      stepCompletionStatus: [false, false, false, false, false],
     },
   };
   
